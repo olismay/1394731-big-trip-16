d309cedd5cadf1caa37f73e0d381487c9a613713
Olga Ismailova <ismailova.o@divan.ru>
2021-12-05 13:21:28 +0300

    Add main layout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { CreateTripInfoTemplate } from './view/trip-info-view';
 import { CreateTripControlsTemplate } from './view/trip-controls-view';
 import { CreateControlsFiltersTemplate } from './view/trip-controls-filter-view';
+import { CreateTripEventsTemplate } from './view/trip-events-view';
+import { CreateTripEventsItemsTemplate } from './view/trip-events-item-view';
 import { renderTemplate, RenderPosition } from './render';
 
 const tabsMainElement = document.querySelector('.page-header');
@@ -8,8 +10,12 @@ const tabsElement = tabsMainElement.querySelector('.trip-main');
 const controlsMainElement = document.querySelector('.trip-main__trip-controls');
 const controlsElement = controlsMainElement.querySelector('.trip-controls__navigation');
 const filtersElement = tabsMainElement.querySelector('.trip-controls__filters');
+const tripEventsMainElement = document.querySelector('.page-body__page-main');
+const tripEventsElement =tripEventsMainElement.querySelector('.trip-events');
 
 
 renderTemplate(tabsElement, CreateTripInfoTemplate(), RenderPosition.AFTERBEGIN);
 renderTemplate(controlsElement, CreateTripControlsTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(filtersElement, CreateControlsFiltersTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(tripEventsElement, CreateTripEventsTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(tripEventsElement, CreateTripEventsItemsTemplate(), RenderPosition.BEFOREEND);
