8f04a803ffbc6b64b7c468296d1c813388970897
Olga Ismailova <ismailova.o@divan.ru>
2021-12-08 21:57:57 +0300

    Add trip points

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { CreateTripInfoTemplate } from './view/trip-info-view';
 import { CreateTripControlsTemplate } from './view/trip-controls-view';
 import { CreateControlsFiltersTemplate } from './view/trip-controls-filter-view';
 import { CreateTripEventsTemplate } from './view/trip-events-view';
-import { CreateTripEventsItemsTemplate } from './view/trip-events-item-view';
+import { CreateTripEventsItemTemplate } from './view/trip-events-item-view';
 import { CreateFormTemplate } from './view/form-add-new-point-view';
+import { CreateListTemplate } from './view/trip-events-list-view';
 import { renderTemplate, RenderPosition } from './render';
 
 const tabsMainElement = document.querySelector('.page-header');
@@ -12,12 +13,17 @@ const controlsMainElement = document.querySelector('.trip-main__trip-controls');
 const controlsElement = controlsMainElement.querySelector('.trip-controls__navigation');
 const filtersElement = tabsMainElement.querySelector('.trip-controls__filters');
 const tripEventsMainElement = document.querySelector('.page-body__page-main');
-const tripEventsElement =tripEventsMainElement.querySelector('.trip-events');
+const tripEventsElement = tripEventsMainElement.querySelector('.trip-events');
 
 
 renderTemplate(tabsElement, CreateTripInfoTemplate(), RenderPosition.AFTERBEGIN);
 renderTemplate(controlsElement, CreateTripControlsTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(filtersElement, CreateControlsFiltersTemplate(), RenderPosition.BEFOREEND);
-renderTemplate(tripEventsElement, CreateFormTemplate(), RenderPosition.AFTERBEGIN);
 renderTemplate(tripEventsElement, CreateTripEventsTemplate(), RenderPosition.AFTERBEGIN);
-renderTemplate(tripEventsElement, CreateTripEventsItemsTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(tripEventsElement, CreateListTemplate(), RenderPosition.BEFOREEND);
+
+const tripList = document.querySelector('.trip-events__list');
+renderTemplate(tripList, CreateFormTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(tripList, CreateTripEventsItemTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(tripList, CreateTripEventsItemTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(tripList, CreateTripEventsItemTemplate(), RenderPosition.BEFOREEND);
--- src/view/form-add-new-point-view.js
+++ src/view/form-add-new-point-view.js
@@ -1,5 +1,6 @@
 export const CreateFormTemplate = () => (
-  ` <form class="event event--edit" action="#" method="post">
+  `<li class="trip-events__item">
+  <form class="event event--edit" action="#" method="post">
   <header class="event__header">
     <div class="event__type-wrapper">
       <label class="event__type  event__type-btn" for="event-type-toggle-1">
@@ -158,5 +159,6 @@ export const CreateFormTemplate = () => (
       </div>
     </section>
   </section>
-</form>`
+</form>
+</li>`
 );
