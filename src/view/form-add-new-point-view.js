417b75f4bbe32f8ab93d81f9e93505b5d6fc234e
Olga Ismailova <ismailova.o@divan.ru>
2022-01-06 23:06:52 +0300

    refactor: Remove console.log

--- src/view/form-add-new-point-view.js
+++ src/view/form-add-new-point-view.js
@@ -1,6 +1,6 @@
 export const CreateFormTemplate = (point) => {
   const { destination, offers } = point;
-  console.log(offers);
+
   return `<li class="trip-events__item">
   <form class="event event--edit" action="#" method="post">
   <header class="event__header">
