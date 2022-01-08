f2db3627de29226cae5310570b43ad42f03c6c31
Olga Ismailova <ismailova.o@divan.ru>
2022-01-08 17:50:11 +0300

    feature: Add createElement function

--- src/render.js
+++ src/render.js
@@ -8,3 +8,10 @@ export const RenderPosition = {
 export const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+export const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
